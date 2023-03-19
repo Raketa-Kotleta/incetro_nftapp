db2d6396bb39c8d150d512cc02016f07b2426411
Raketa-Kotleta <niceikis@gmail.com>
2023-03-19 12:15:44 +0300

    first commit

---
+++ src/modules/navbar.js
@@ -0,0 +1,10 @@
+import Logo from '../assets/logo.png';
+import '../assets/lang-planet-icon.png'
+import '../assets/lang-toggle-icon.png'
+const NavBar = document.querySelector('.navbar');
+const Toggler = NavBar.querySelector('.toggle');
+const ToggableItem = NavBar.querySelector('#'+Toggler.getAttribute('target'));
+Toggler.onclick = function(){
+    ToggableItem.classList.toggle('active')
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: [
+        path.join(__dirname,'src', 'modules', 'landing.js'),
+        path.join(__dirname,'src', 'modules', 'navbar.js')
+    ],
+    output: {
+        path: path.join(__dirname, "dist"),
+        publicPath: "/",
+        filename: "landing-page.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+           {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+           },
+           {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            },
+           {
+            test: /\.(html)$/,
+            use: ['html-loader']
+            }
+          
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname,'src','templates','index.html'),
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'src','templates'),
+        port: 8080,
+        hot: true,
+    },
+}
